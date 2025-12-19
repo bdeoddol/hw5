f00e3212fefc98dd47a8412f98fb54b884c05ea0
Nhan Tong <nktong@usc.edu>
2025-12-19 11:03:02 +0000

    submit

--- schedwork.cpp
+++ schedwork.cpp
@@ -21,6 +21,7 @@ static const Worker_T INVALID_ID = (unsigned int)-1;
 
 
 // Add prototypes for any helper functions here
+bool dfs(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftCount, size_t slot);
 
 
 // Add your implementation of schedule() and other helper functions here
@@ -38,8 +39,47 @@ bool schedule(
     sched.clear();
     // Add your code below
 
+    size_t numDays = avail.size();
+    size_t numWorkers = avail[0].size();
 
+    sched.resize(numDays, vector<Worker_T>(dailyNeed, INVALID_ID));
+    vector<size_t>shiftCount(numWorkers, 0);
 
+    bool result = dfs(avail, dailyNeed, maxShifts, sched, shiftCount, 0);
+    return result;
 
 }
 
+bool dfs(const AvailabilityMatrix& avail, size_t dailyNeed, size_t maxShifts, DailySchedule& sched, vector<size_t>& shiftCount, size_t slot){
+  size_t numDays = avail.size();
+  size_t numWorkers = avail[0].size();
+
+  if(slot == numDays * dailyNeed){
+    return true;
+
+  }
+
+  size_t day = slot/ dailyNeed;
+  size_t position = slot % dailyNeed;
+
+  for(size_t i = 0;i<numWorkers; i++ ){
+    //check for good conditions first
+    if(avail[day][i] == false || shiftCount[i] >= maxShifts){
+      continue;
+    }
+    else if(std::find(sched[day].begin(), sched[day].end(), i) != sched[day].end()){
+      continue;
+    }
+    sched[day][position] = i;
+    shiftCount[i]++;
+
+    if(dfs(avail, dailyNeed, maxShifts, sched, shiftCount, slot+1)){
+      return true;
+    }
+
+    shiftCount[i]--;
+    sched[day][position] = INVALID_ID;
+  }
+
+  return false; //return false by default
+}
--- wordle.cpp
+++ wordle.cpp
@@ -13,7 +13,7 @@ using namespace std;
 
 
 // Add prototypes of helper functions here
-
+void dfs(size_t index, std::string in, std::string floating, std::set<std::string>&results, const std::set<std::string>& dict);
 
 // Definition of primary wordle function
 std::set<std::string> wordle(
@@ -22,7 +22,53 @@ std::set<std::string> wordle(
     const std::set<std::string>& dict)
 {
     // Add your code here
+    std::set<std::string> results;
+    dfs(0, in, floating, results, dict);
+    return results;
 
 }
 
 // Define any helper functions here
+
+
+void dfs(size_t index, std::string in, std::string floating, std::set<std::string>&results, const std::set<std::string>& dict){
+  if(index == in.size()){
+    if(dict.count(in) == 1 && floating.size() == 0){
+      results.insert(in);
+
+    }
+    return;
+  }
+
+  size_t slots = 0;
+  for(size_t i = index; i < in.size(); i++){
+    if(in[i] == '-'){
+      slots++;
+    }
+
+  }
+
+  if(slots < floating.size()){
+    return;
+
+  }
+
+  if(in[index] != '-'){
+    dfs(index+1, in, floating, results, dict);
+
+  }
+  else{
+    for(char c = 'a'; c<= 'z'; c++){
+      in[index] = c;
+      std::string floating_ = floating;
+      size_t pos = floating_.find(c);
+      if(pos != std::string::npos){
+        floating_.erase(pos,1);
+      }
+      dfs(index+1, in, floating_,results, dict);
+
+      in[index] = '-';
+    }
+  }
+
+}
